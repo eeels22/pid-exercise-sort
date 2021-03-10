07bd2502c6d0ddd2d6bfdc214dfc165d56491b47
En-Chi Liu <enchi.liu@gmail.com>
2021-03-10 11:28:15 +0100

    Implemented SelectionSort and BubbleSort

--- src/main/java/pid/exercise/sort/BubbleSort.java
+++ src/main/java/pid/exercise/sort/BubbleSort.java
@@ -3,7 +3,25 @@ package pid.exercise.sort;
 public class BubbleSort {
 
     public static void sort(int[] array) {
-        // TODO Implement bubble sort
+        int maxIndex = array.length - 1;
+        // the left comparator starts at index 0, and increments by one
+        // as the highest value is placed at the end of the array,
+        // we loop until we reach the index 2 before the last verified ordered position
+        for (int i = 0; i < maxIndex - 1 - i; i++) {
+            // the right comparator starts at index 0, and increments by one
+            // as the highest value is placed at the end of the array,
+            // we loop until we reach the index before the last verified ordered position
+            for (int j = i + 1; j < maxIndex -1; j++){ // right comparator
+                if (array[i] > array[j]) { // if left position is higher, swap
+                    int temp = array[i];
+                    array[i] = array[j];
+                    array[j] = temp;
+                }
+            }
+        }
     }
 
+
 }
+
+
--- src/main/java/pid/exercise/sort/SelectionSort.java
+++ src/main/java/pid/exercise/sort/SelectionSort.java
@@ -3,7 +3,16 @@ package pid.exercise.sort;
 public class SelectionSort {
 
     public static void sort(int[] array) {
-        // TODO Implement selection sort
+        for (int i = 0; i < (array.length - 1); i++) { // start at index 0, increment until penultimate index
+            int min = i; // set min to current outer loop index
+            for (int j = i + 1; j < array.length; j++) { // start at index after outer loop index, increment until last index
+                if (array[j] <= array[min]) { // if the current inner index is less than the min (outer index)
+                    min = j; // set the min to the inner index
+                }
+            }
+            int temp = array[i]; // store the outer index value, before...
+            array[i] = array[min]; // swapping the outer index value with the min value found during looping
+            array[min] = temp;
+        }
     }
-
 }
